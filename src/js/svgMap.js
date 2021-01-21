7374f60751f23edd41c668db1c640d2cbb1f1d90
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2021-01-21 16:03:36 +0300

    Updates code formatting

--- src/js/svgMap.js
+++ src/js/svgMap.js
@@ -1,75 +1,72 @@
 // Wrapper function
-var svgMap = function (options) {
-  this.init(options)
+var svgMap = function(options) {
+	this.init(options)
 };
 
 // Initialize SVG Worldmap
-svgMap.prototype.init = function (options) {
+svgMap.prototype.init = function(options) {
+	// Default options, pass a custom options object to overwrite specific
+	var defaultOptions = {
+		// The id of the element to render the map in
+		targetElementID: '',
 
-  // Default options, pass a custom options object to overwrite specific
-  var defaultOptions = {
+		// The element to render the map in
+		targetElement: null,
 
-    // The element to render the map in
-    targetElementID: '',
+		// Minimum and maximum zoom
+		minZoom: 1,
+		maxZoom: 25,
 
-    // Minimum and maximum zoom
-    minZoom: 1,
-    maxZoom: 25,
+		// Initial zoom
+		initialZoom: 1.06,
 
-    // Initial zoom
-    initialZoom: 1.06,
+		// Zoom sensitivity
+		zoomScaleSensitivity: 0.2,
 
-    // Zoom sensitivity
-    zoomScaleSensitivity: 0.2,
+		// Zoom with mousewheel
+		mouseWheelZoomEnabled: true,
 
-    // Zoom with mousewheel
-    mouseWheelZoomEnabled: true,
+		// Data colors
+		colorMax: '#CC0033',
+		colorMin: '#FFE5D9',
+		colorNoData: '#E2E2E2',
 
-    // Data colors
-    colorMax: '#CC0033',
-    colorMin: '#FFE5D9',
-    colorNoData: '#E2E2E2',
+		// The flag type can be 'image' or 'emoji'
+		flagType: 'image',
 
-    // The flag type can be 'image' or 'emoji'
-    flagType: 'image',
+		// The URL to the flags when using flag type 'image', {0} will get replaced with the lowercase country id
+		flagURL: 'https://cdn.jsdelivr.net/gh/hjnilsson/country-flags@latest/svg/{0}.svg',
 
-    // The URL to the flags when using flag type 'image', {0} will get replaced with the lowercase country id
-    flagURL: 'https://cdn.jsdelivr.net/gh/hjnilsson/country-flags@latest/svg/{0}.svg',
+		// Decide whether to show the flag option or not
+		hideFlag: false,
 
-    // Decide whether to show the flag option or not
-    hideFlag: false,
+		// The default text to be shown when no data is present
+		noDataText: 'No data available',
 
-    // The default text to be shown when no data is present
-    noDataText: 'No data available',
+		// Country specific options
+		countries: {
+			// Western Sahara: Set to false to combine Morocco (MA) and Western Sahara (EH)
+			EH: true
+		}
+	};
 
-    // Country specific options
-    countries: {
-      // Western Sahara: Set to false to combine Morocco (MA) and Western Sahara (EH)
-      EH: true
-    }
-  };
+	this.options = Object.assign({}, defaultOptions, (options || {}));
 
-  this.options = Object.assign({}, defaultOptions, (options || {}));
+	// Abort if target element not found
+	if (!this.options.targetElementID || !document.getElementById(this.options.targetElementID)) this.error('Target element not found');
 
-  // Abort if target element not found
-  if (!this.options.targetElementID || !document.getElementById(this.options.targetElementID)) {
-    this.error('Target element not found');
-  }
+	// Abort if no data
+	this.options.data || this.error('No data');
 
-  // Abort if no data
-  if (!this.options.data) {
-    this.error('No data');
-  }
+	// Global id
+	this.id = this.options.targetElementID;
 
-  // Global id
-  this.id = this.options.targetElementID;
+	// Cache wrapper element
+	this.wrapper = document.getElementById(this.options.targetElementID);
 
-  // Cache wrapper element
-  this.wrapper = document.getElementById(this.options.targetElementID);
+	// Create the map
+	this.createMap();
 
-  // Create the map
-  this.createMap();
-
-  // Apply map data
-  this.applyData(this.options.data);
+	// Apply map data
+	this.applyData(this.options.data);
 };
--- src/js/umd.js
+++ src/js/umd.js
@@ -1,15 +1,14 @@
 // UMD module definition
-(function (window, document) {
+(function(window, document) {
+	// AMD
+	if (typeof define === 'function' && define.amd) {
+		define('svgMap', function() {
+			return svgMap;
+		});
 
-  // AMD
-  if (typeof define === 'function' && define.amd) {
-    define('svgMap', function () {
-      return svgMap;
-    });
-
-  // CMD
-  } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = svgMap;
-    window.svgMap = svgMap;
-  }
+		// CMD
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = svgMap;
+		window.svgMap = svgMap;
+	}
 })(window, document)
